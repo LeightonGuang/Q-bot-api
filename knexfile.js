695c34e606a7a967a43c78e52286026be5d5c9ea
Leighton Guang <leighton.guang@icloud.com>
2024-02-26 11:20:38 +0000

    fix: use .env variables

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 // const dotenv = require("dotenv");
 import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+dotenv.config({ path: "./.env" });
 
 const config = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: "QBot",
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
