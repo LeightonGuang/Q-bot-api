28b8eecbd4e931b3ccdd88301110fb183375013f
Leighton Guang <leighton.guang@icloud.com>
2024-02-26 11:20:07 +0000

    feat: add user router

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import express from "express";
-const app: express.Application = express();
+import userRouter from "./routes/user-routes.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 const PORT: number | string = process.env.PORT || 5050;
+const app: express.Application = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Q-bot-api");
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
