eba4b2d689a0a04e8f933c222a72e7c79f578db7
Leighton Guang <leighton.guang@icloud.com>
2024-02-27 06:13:54 +0000

    feat: populate seed data for riot accounts

--- seeds/populate.js
+++ seeds/populate.js
@@ -1,6 +1,12 @@
-import users from "../seed-data/users.js";
+const users = require("../seed-data/accounts.js/index.js");
+const riot_accounts = require("../seed-data/riot_accounts.js");
 
-export const seed = async (knex) => {
+exports.seed = async (knex) => {
   await knex("users").del();
   await knex("users").insert(users);
+
+  await knex("riot_accounts").del();
+  await knex("riot_accounts").insert(riot_accounts);
+
+  // await knex("steam_accounts").del();
 };
