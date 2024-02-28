d7ba9258a6354468279bbf778bd87ae6ce200e4b
Leighton Guang <leighton.guang@icloud.com>
2024-02-28 18:01:09 +0000

    feat: create route for getting riot accounts and steam accounts

--- routes/account-routes.ts
+++ routes/account-routes.ts
@@ -4,6 +4,7 @@ import {
   getAllUsers,
   getAllAccountsByDiscordId,
   getRiotAccountsByDiscordId,
+  getSteamAccountsByDiscordId,
 } from "../controllers/account-controller";
 
 accountRouter.route("/").get(getAllUsers);
@@ -11,8 +12,10 @@ accountRouter.route("/").get(getAllUsers);
 accountRouter.route("/:discord_id").get(getAllAccountsByDiscordId);
 // accountRouter.route("/riot/add/:discord_id").post(addRiotAccount);
 // accountRouter.route("/steam/add/:discord_id").post(addSteamAccount);
+// select sub command
+accountRouter.route("/riot/get/:discord_id").get(getRiotAccountsByDiscordId);
 // accountRouter.route("/riot/edit/:discord_id").put(editRiotAccount);
-// accountRouter.route("/riot/edit/:discord_id").put(editRiotAccount);
+accountRouter.route("/steam/get/:discord_id").get(getSteamAccountsByDiscordId);
 // accountRouter.route("/steam/edit/:discord_id").put(editSteamAccount);
 
 export default accountRouter;
