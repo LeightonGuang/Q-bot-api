bcab0fb5b8037b40ac465c988af9ece93c226347
Leighton Guang <leighton.guang@icloud.com>
2024-03-01 07:58:16 +0000

    feat: add steam id to steam accounts table

--- migrations/20240226074146_create_user_table.js
+++ migrations/20240226074146_create_user_table.js
@@ -22,6 +22,7 @@ exports.up = (knex) => {
     .createTable("steam_accounts", (table) => {
       table.increments("id").primary();
       table.string("discord_id").notNullable();
+      table.string("steam_id").notNullable();
       table.string("account_name").notNullable();
       table.integer("friend_code").notNullable();
       table.string("steam_profile_url").notNullable();
