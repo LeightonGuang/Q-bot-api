a3976b19c15bb4af8796cbafdaa858763b9622a7
Leighton Guang <leighton.guang@icloud.com>
2024-03-02 18:20:42 +0000

    feat: rename routes and new add account route

--- routes/account-routes.ts
+++ routes/account-routes.ts
@@ -3,27 +3,29 @@ const accountRouter: Router = Router();
 import {
   getAllUsers,
   getAllAccountsByDiscordId,
+  addRiotAccount,
+  addSteamAccount,
   getRiotAccountsByDiscordId,
-  selectRiotAccountByRiotId,
+  selectRiotAccount,
   getSteamAccountsByDiscordId,
-  selectSteamAccountBySteamId,
-  deleteRiotAccountByRiotId,
-  deleteSteamAccountBySteamId,
+  selectSteamAccount,
+  deleteRiotAccount,
+  deleteSteamAccount,
 } from "../controllers/account-controller";
 
 accountRouter.route("/").get(getAllUsers);
 // accountRouter.route("/:id").get(getUserByDiscordId);
 accountRouter.route("/:discord_id").get(getAllAccountsByDiscordId);
-// accountRouter.route("/riot/add/:discord_id").post(addRiotAccount);
-// accountRouter.route("/steam/add/:discord_id").post(addSteamAccount);
+accountRouter.route("/riot/add").post(addRiotAccount);
+accountRouter.route("/steam/add").post(addSteamAccount);
 // select sub command
 accountRouter.route("/riot/get/:discord_id").get(getRiotAccountsByDiscordId);
-accountRouter.route("/riot/select").patch(selectRiotAccountByRiotId);
+accountRouter.route("/riot/select").patch(selectRiotAccount);
 accountRouter.route("/steam/get/:discord_id").get(getSteamAccountsByDiscordId);
-accountRouter.route("/steam/select").patch(selectSteamAccountBySteamId);
+accountRouter.route("/steam/select").patch(selectSteamAccount);
 
 // delete sub command
-accountRouter.route("/riot/delete").delete(deleteRiotAccountByRiotId);
-accountRouter.route("/steam/delete").delete(deleteSteamAccountBySteamId);
+accountRouter.route("/riot/delete").delete(deleteRiotAccount);
+accountRouter.route("/steam/delete").delete(deleteSteamAccount);
 
 export default accountRouter;
