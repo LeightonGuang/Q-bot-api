a2d48975a5b9650f29a5cf818783792106b57ea6
Leighton Guang <leighton.guang@icloud.com>
2024-03-12 22:14:54 +0000

    test: add unit test for getting all accounts of a user, active valorant account and if user is not registered

--- tests/get.test.js
+++ tests/get.test.js
@@ -2,7 +2,36 @@ const axios = require("axios");
 const { test, describe, expect } = require("@jest/globals");
 
 describe("GET requests", () => {
-  test("GET boolean if the account is registered /api/account/registered/508402815157403668", async () => {
+  // account routes
+  test("GET all accounts from 508402815157403668 discord id", async () => {
+    const { data, status } = await axios.get(
+      "http://localhost:8080/api/account/508402815157403668"
+    );
+
+    expect(status).toBe(200);
+    data.riotAccountList.forEach((riotAccount) => {
+      expect(typeof riotAccount).toBe("object");
+      expect(Object.keys(riotAccount)).toContain("id");
+      expect(Object.keys(riotAccount)).toContain("discord_id");
+      expect(Object.keys(riotAccount)).toContain("riot_id");
+      expect(Object.keys(riotAccount)).toContain("region");
+      expect(Object.keys(riotAccount)).toContain("rank");
+      expect(Object.keys(riotAccount)).toContain("active");
+    });
+
+    data.steamAccountList.forEach((steamAccount) => {
+      expect(typeof steamAccount).toBe("object");
+      expect(Object.keys(steamAccount)).toContain("id");
+      expect(Object.keys(steamAccount)).toContain("discord_id");
+      expect(Object.keys(steamAccount)).toContain("steam_id");
+      expect(Object.keys(steamAccount)).toContain("account_name");
+      expect(Object.keys(steamAccount)).toContain("friend_code");
+      expect(Object.keys(steamAccount)).toContain("steam_profile_url");
+      expect(Object.keys(steamAccount)).toContain("active");
+    });
+  });
+
+  test("GET true if 508402815157403668 is registered", async () => {
     const { data, status } = await axios.get(
       "http://localhost:8080/api/account/registered/508402815157403668"
     );
@@ -11,7 +40,16 @@ describe("GET requests", () => {
     expect(data).toEqual(true);
   });
 
-  test("GET all riot accounts from discord id /api/account/riot/get/508402815157403669", async () => {
+  test("GET false if a user is not registered", async () => {
+    const { data, status } = await axios.get(
+      "http://localhost:8080/api/account/registered/123456789012345678"
+    );
+
+    expect(status).toBe(200);
+    expect(data).toEqual(false);
+  });
+
+  test("GET all riot accounts from 508402815157403669 discord id", async () => {
     const { data, status } = await axios.get(
       "http://localhost:8080/api/account/riot/get/508402815157403669"
     );
@@ -28,7 +66,7 @@ describe("GET requests", () => {
     });
   });
 
-  test("GET all steam accounts from discord id api/account/steam/get/508402815157403669", async () => {
+  test("GET all steam accounts from 508402815157403669 discord id", async () => {
     const { data, status } = await axios.get(
       "http://localhost:8080/api/account/steam/get/508402815157403669"
     );
@@ -43,6 +81,24 @@ describe("GET requests", () => {
       expect(Object.keys(steamAccount)).toContain("friend_code");
       expect(Object.keys(steamAccount)).toContain("steam_profile_url");
       expect(Object.keys(steamAccount)).toContain("active");
+    });
+  });
+
+  // valorant routes
+  test("GET active valorant accounts from 508402815157403669 discord id", async () => {
+    const { data, status } = await axios.get(
+      "http://localhost:8080/api/valorant/active/get/508402815157403669"
+    );
+
+    expect(status).toBe(200);
+    data.forEach((valorantAccount) => {
+      expect(typeof valorantAccount).toBe("object");
+      expect(Object.keys(valorantAccount)).toContain("id");
+      expect(Object.keys(valorantAccount)).toContain("discord_id");
+      expect(Object.keys(valorantAccount)).toContain("riot_id");
+      expect(Object.keys(valorantAccount)).toContain("region");
+      expect(Object.keys(valorantAccount)).toContain("rank");
+      expect(Object.keys(valorantAccount)).toContain("active");
     });
   });
 });
